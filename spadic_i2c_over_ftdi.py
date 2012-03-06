1fcbcfc27f38a40f9c0f44553114e2c9bffeb2a2
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2012-03-06 15:26:37 +0100

    read & write verbessert

--- spadic_i2c_over_ftdi.py
+++ spadic_i2c_over_ftdi.py
@@ -1,29 +1,46 @@
 #!/usr/bin/python
 
+import sys
 import ftdi
 # http://www.intra2net.com/en/developer/libftdi/documentation/group__libftdi.html
-#import usb
 
-VID = 0x0403;
-PID = 0x6010;
+class FtdiOpenError(Exception):
+    pass
 
-class SPADIC_I2C_RF:
+class SpadicI2cRf:
     def __init__(self):
-        self.ftdic = ftdi.ftdi_context()
-        ftdi.ftdi_init(self.ftdic)
-        ftdi.ftdi_usb_open(self.ftdic, VID, PID)
-        ftdi.ftdi_set_bitmode(self.ftdic, 0, ftdi.BITMODE_SYNCFF)
+        self.ftdic = self._connect()
+
+    def _connect(self, VID=0x0403, PID=0x6010):
+        context = ftdi.ftdi_context()
+        ftdi.ftdi_init(context)
+        if not (ftdi.ftdi_usb_open(context, VID, PID) == 0):
+            self.ftdic = None
+        else:
+            ftdi.ftdi_set_bitmode(context, 0, ftdi.BITMODE_SYNCFF)
+            self.ftdic = context
 
     def _write_data(self, byte_list):
-        ftdi.ftdi_write_data(self.ftdic, ''.join(map(chr, byte_list)),
-                             len(byte_list))
+        bytes_left = byte_list
+        while bytes_left:
+            num_bytes_written = ftdi.ftdi_write_data(self.ftdic,
+                                ''.join(map(chr, bytes_left)), len(bytes_left))
+            bytes_left = bytes_left[num_bytes_written:]
 
     def _read_data(self, num_bytes):
         buf = chr(0)*num_bytes
-        ftdi.ftdi_read_data(self.ftdic, buf, num_bytes))
+        bytes_read = []
+        while buf:
+            num_bytes_read = ftdi.ftdi_read_data(self.ftdic, buf, len(buf))
+            bytes_read += map(ord, buf[:num_bytes_read])
+            buf = buf[num_bytes_read:]
         return map(ord, buf)
 
 
 if __name__=='__main__':
-    s = SPADIC_I2C_RF()
+    s = SpadicI2cRf()
+
+    if s.ftdic is None:
+        sys.exit('could not connect to device!')
+    
 
