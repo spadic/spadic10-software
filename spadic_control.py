ba537aa99391eebfca885663b322fe7398f7faf2
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2012-03-08 19:04:46 +0100

    made demo for sweeping a DAC value

--- spadic_control.py
+++ spadic_control.py
@@ -4,18 +4,23 @@ from spadic_control_lib import *
 
 
 if __name__=='__main__':
-    #if len(sys.argv < 3):
-    #    sys.exit('usage: %s <reg_addr> <value>' % sys.argv[0])
-
-    #address = int(sys.argv[1], 16)
-    #value = int(sys.argv[2], 16)
-
     s = SpadicI2cRf()
+    print 'opened USB connection.'
+
+    # switch LED on
+    s.write_register(RF_MAP['overrides'], 0x20)
+
     sr = SpadicShiftRegister()
 
-    sr.set_value('dac1', 67)
-    s.write_shiftregister(str(sr))
+    # sweep nCascN
+    for v in [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]:
+        raw_input('Setting nCascN to %i. Press <Enter> to confirm.' % v)
+        sr.set_value('nCascN', v)
+        s.write_shiftregister(str(sr))
 
-    # try to switch some LEDs on
-    s.write_register(RF_MAP['overrides'], 0x20)
+    # switch LED off
+    s.write_register(RF_MAP['overrides'], 0x00)
+
+    del s # not really necessary, should be done automatically
+    print 'closed USB connection.'
 
