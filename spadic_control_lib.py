c73b578b9f271e8f089d46c5467b6515a9bccf07
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2012-04-17 17:07:07 +0200

    changed read_data_test_output to work with IOM package mode, implemented timeout with SIGALRM, updated ftdireadtest with sleep-based timeout, added some convenience functions

--- spadic_control_lib.py
+++ spadic_control_lib.py
@@ -1,6 +1,20 @@
+import signal
+
 from iom_lib import *
 from spadic_registers import *
 from bit_byte_helper import *
+
+
+#====================================================================
+# prepare timeout
+#====================================================================
+class TimeoutException(Exception):
+    pass
+
+# set timeout handler for SIGALRM to raise TimeoutException
+def timeout_handler(signum, frame):
+    raise TimeoutException()
+signal.signal(signal.SIGALRM, timeout_handler)
 
 
 #====================================================================
@@ -12,6 +26,10 @@ from bit_byte_helper import *
 #--------------------------------------------------------------------
 IOM_ADDR_I2C = 0x20
 IOM_ADDR_TDO = 0x30
+
+# size of package parts in IOM package mode
+# first byte is always 0xFF --> discard after read
+PACKAGE_SIZE = 9
 
 
 #====================================================================
@@ -70,16 +88,26 @@ class Spadic(FtdiIom):
         self.write_shiftregister(str(sr))
 
     #----------------------------------------------------------------
-    # read data from test output
+    # read data from test output -> IOM -> FTDI
+    # (assuming package mode is used)
     #----------------------------------------------------------------
-    def read_data_test_output(self, num_bytes, block_size=255):
+    def read_data_test_output(self, num_bytes, timeout=1):
+        # read data until num_bytes are read
+        # abort if timeout is over after no data was received
         num_bytes_left = num_bytes
-        while num_bytes_left > 0: # negative numbers are treated as True
-            bytes_read = self._iom_read(IOM_ADDR_TDO,
-                                        min(num_bytes_left, block_size))
+        while num_bytes_left >= PACKAGE_SIZE:
+            signal.alarm(timeout) # set alarm
+            try:
+                bytes_read = self._ftdi_read(PACKAGE_SIZE)[1:]
+                             # discard the first byte
+            except TimeoutException:
+                break
+            finally:
+                signal.alarm(0) # disable alarm in any case
+
             for byte in bytes_read:
                 yield byte
-            num_bytes_left -= block_size
+            num_bytes_left -= len(bytes_read)
             
 
 # prepare some stuff that is frequently used
@@ -124,17 +152,28 @@ def config_ftdireadtest():
     testout(1)
     enablechannel0(1)
     selectmask(0xFFFF0000) # first 16 samples
+    s.write_register(48, 0) # diffmode off
     s.write_register(8, 0x00)
 
 def randdata(n):
     return [random.randint(0, 120) for i in range(n)]
+
+def writedata():
+    s._iom_write(0x30, [0x7F, 0x55, 0x66])
     
-def ftdireadtest(f=None):
+def ftdireadtest(f=None, max_timeout=1, timeout_init=1e-6):
     start = time.time()
+    timeout = timeout_init
     print >> f, ''
     while True:
         d = s._ftdi_read(9, 1)
         if len(d):
+            timeout = timeout_init
             print >> f, '%6.1f: ' % (time.time()-start) + \
                         ' '.join('%02X' % x for x in d)
+        else:
+            if timeout > max_timeout:
+                break
+            time.sleep(timeout)
+            timeout = 2*timeout
     
