2456b84051733ab23732385dd08fd971f38ca2f8
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2012-10-28 22:06:49 +0100

    added dummy mode... quickly hacked in, probably needs patching

--- spadic/__init__.py
+++ spadic/__init__.py
@@ -1,4 +1,4 @@
-from basic import Spadic
+from basic import Spadic, SpadicDummy
 from message import MessageReader
 from register import RegisterFile, ShiftRegister
 from control import Controller
@@ -8,6 +8,6 @@ from control import Controller
 __version__ = '0.9.4'
 
 __all__ = [
-    'Spadic', 'MessageReader', 'RegisterFile', 'ShiftRegister', 'Controller'
+    'Spadic', 'SpadicDummy', 'MessageReader', 'RegisterFile', 'ShiftRegister', 'Controller'
 ]
 
