4438fd21d4e203137f37871c612ffa9c243d1c3c
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-04-18 14:13:33 +0200

    re-implemented message data/metadata extraction

--- spadic/__init__.py
+++ spadic/__init__.py
@@ -1,12 +1,12 @@
 from basic import Spadic
-from register import RegisterFile, ShiftRegister
+from message import Message
 from control import Controller
 
-#del basic, message, register, control
+#del basic, message, control
 
 __version__ = '0.9.6'
 
 __all__ = [
-    'Spadic', 'RegisterFile', 'ShiftRegister', 'Controller'
+    'Spadic', 'Message', 'Controller'
 ]
 
--- spadic/message.py
+++ spadic/message.py
@@ -1,9 +1,7 @@
-from bit_byte_helper import int2bitstring
+def match_word(word, (value, mask)):
+    """Test if a part of a word matches a given value."""
+    return word & mask == value
 
-
-#====================================================================
-# SPADIC message definitions
-#====================================================================
 
 #--------------------------------------------------------------------
 # preambles (value, mask)
@@ -21,25 +19,25 @@ preamble = {
 }
 
 #--------------------------------------------------------------------
-# end of message types
+# stop types (value, mask)
 #--------------------------------------------------------------------
-endofmessage = {
-  'eEND': (0x0000, 0x0007),
-  'eEBF': (0x0001, 0x0007),
-  'eEFF': (0x0002, 0x0007),
-  'eEDH': (0x0003, 0x0007),
-  'eEDB': (0x0004, 0x0007),
-  'eEDO': (0x0005, 0x0007),
-  'eXX1': (0x0006, 0x0007), # unused
-  'eXX2': (0x0007, 0x0007)  # unused
+stoptype = {
+  'sEND': (0x0000, 0x0007),
+  'sEBF': (0x0001, 0x0007),
+  'sEFF': (0x0002, 0x0007),
+  'sEDH': (0x0003, 0x0007),
+  'sEDB': (0x0004, 0x0007),
+  'sEDO': (0x0005, 0x0007),
+  'sXX1': (0x0006, 0x0007), # unused
+  'sXX2': (0x0007, 0x0007)  # unused
 }
-endofmessage_str = {
-  'eEND': 'normal end of message',
-  'eEBF': 'buffer full',
-  'eEFF': 'ordering fifo full',
-  'eEDH': 'multi hit',
-  'eEDB': 'multi hit but buffer full',
-  'eEDO': 'multi hit but ordering fifo full'
+stoptype_str = {
+  0: 'normal end of message',
+  1: 'channel buffer full',
+  2: 'ordering fifo full',
+  3: 'multi hit',
+  4: 'multi hit and channel buffer full',
+  5: 'multi hit and ordering fifo full'
 }
 
 #--------------------------------------------------------------------
@@ -64,29 +62,29 @@ infotype = {
   'iXXB': (0x0F00, 0x0F00)  # unused
 }
 infotype_str = {
-  'iDIS': 'disable channel during message readout',
-  'iNGT': 'next grant timeout',
-  'iNRT': 'next request timeout',
-  'iNBE': 'new grant but channel empty',
-  'iMSB': 'corruption in message builder',
-  'iNOP': 'empty word',
-  'iSYN': 'out of sync'
+  0: 'disable channel during message readout',
+  1: 'next grant timeout',
+  2: 'next request timeout',
+  3: 'new grant but channel empty',
+  4: 'corruption in message builder',
+  5: 'empty word',
+  6: 'out of sync'
 }
 
 #--------------------------------------------------------------------
-# hit types
+# hit types (value, mask)
 #--------------------------------------------------------------------
 hittype = {
-  'eNON': (0x0000, 0x0030),
-  'eINT': (0x0010, 0x0030),
-  'eEXT': (0x0020, 0x0030),
-  'eIAE': (0x0030, 0x0030) 
+  'hGLB': (0x0000, 0x0030),
+  'hSLF': (0x0010, 0x0030),
+  'hNBR': (0x0020, 0x0030),
+  'hSAN': (0x0030, 0x0030) 
 }
 hittype_str = {
-  'eNON': 'no hit, error',
-  'eINT': 'internal hit',
-  'eEXT': 'external hit',
-  'eIAE': 'int+ext hit'
+  0: 'global trigger',
+  1: 'self triggered',
+  2: 'neighbor triggered',
+  3: 'self and neighbor triggered'
 }
 
 
@@ -95,17 +93,16 @@ hittype_str = {
 # message reconstruction & interpretation
 #====================================================================
 
-def match_word(word, (value, mask)):
-    return word & mask == value
-
 #--------------------------------------------------------------------
 # split sequence of message words into messages (or info words)
 #--------------------------------------------------------------------
 class MessageSplitter:
+    """Splits a stream of message words into individual messages."""
     def __init__(self):
         self._remainder = []
 
     def __call__(self, message_words):
+        """Feed new message words."""
         # recall remainder from the last time
         message = self._remainder
 
@@ -135,119 +132,97 @@ class MessageSplitter:
 
 
 #--------------------------------------------------------------------
-# Message class
+# extract information from messages
 #--------------------------------------------------------------------
 class Message():
-    group_id              = None
-    channel_id            = None
-    timestamp             = None
-    data                  = None
-    num_data              = None
-    hit_type              = None
-    stop_type             = None
-    buffer_overflow_count = None
-    epoch_count           = None
-    info_type             = None
-    _valid                = True
+    """Representation of a SPADIC 1.0 message."""
 
-    def __init__(self, message): # message = output of messages()
-        self.words = message     # i.e. some raw 16-bit words
+    def __init__(self, words):
+        """Extract the metadata from the message."""
+        self.group_id              = None
+        self.channel_id            = None
+        self.timestamp             = None
+        self._data                 = None
+        self.num_data              = None
+        self.hit_type              = None
+        self.stop_type             = None
+        self.buffer_overflow_count = None
+        self.epoch_count           = None
+        self.info_type             = None
 
-        #------------------------------------------------------------
-        # get start of message (group/channel IDs)
-        #------------------------------------------------------------
-        start_words = [word for word in message
-                       if word.startswith(preamble['wSOM'])]
-        if start_words:
-            w = start_words[0]
-            self.group_id   = int(w[4:12], 2)
-            self.channel_id = int(w[12:16], 2)
+        self.words = words
 
-        #------------------------------------------------------------
-        # get timestamp
-        #------------------------------------------------------------
-        timestamp_words = [word for word in message
-                           if word.startswith(preamble['wTSW'])]
-        if timestamp_words:
-            w = timestamp_words[0]
-            self.timestamp = int(w[len(preamble['wTSW']):], 2)
+        for w in words:
+            # start of message -> group ID, channel IDs
+            if match_word(w, preamble['wSOM']):
+                self.group_id   = (w & 0x0FF0) >> 4
+                self.channel_id = (w & 0x000F)
 
-        #------------------------------------------------------------
-        # get raw data
-        #------------------------------------------------------------
-        rda_pos = [i for (i, word) in enumerate(message)
-                   if word.startswith(preamble['wRDA'])]
+            # timestamp
+            elif match_word(w, preamble['wTSW']):
+                self.timestamp = (w & 0x0FFF)
 
-        if rda_pos:
-            p = rda_pos[0]
+            # end of message -> num. data, hit type, stop type
+            elif match_word(w, preamble['wEOM']):
+                self.num_data = (w & 0x0FC0) >> 6
+                self.hit_type = (w & 0x0030) >> 4
+                self.stop_type = (w & 0x0007)
 
-            # data begins with the rest of the 'start of raw data' word
-            data_string = message[p][len(preamble['wRDA']):]
+            # buffer overflow count
+            elif match_word(w, preamble['wBOM']):
+                self.buffer_overflow_count = (w & 0x00FF)
 
-            # data continues for all consecutive words with continuation marker
-            for word in message[p+1:]:
-                if word.startswith(preamble['wCON']):
-                    data_string += word[len(preamble['wCON']):]
-                else:
-                    break
+            # epoch marker
+            elif match_word(w, preamble['wEPM']):
+                self.epoch_count = (w & 0x0FFF)
 
-            # split data in 9 bit values and interpret as 2-s complement
-            self.data = [x-512 if x > 255 else x
-                         for x in [int(data_string[i*9:(i+1)*9], 2)
-                                   for i in range(len(data_string)/9)]]
+            # info words
+            elif match_word(w, preamble['wINF']):
+                self.info_type = (w & 0x0F00) >> 8
+                if any(match_word(w, infotype[it])
+                       for it in ['iDIS', 'iNGT', 'iNBE', 'iMSB']):
+                    self.channel_id = (w & 0x00F0) >> 4
 
-        #------------------------------------------------------------
-        # get end of message
-        #------------------------------------------------------------
-        end_words = [word for word in message
-                     if any(word.startswith(preamble[p])
-                            for p in ['wEOM', 'wBOM', 'wEPM'])]
 
-        if end_words:
-            w = end_words[0]
+    def _update_data(self):
+        """Extract the data samples from the message."""
+        def iter_data():
+            r = 0 # data buffer
+            pos = -9 # initial position of the mask LSB
+            for w in self.words:
+                if pos < 0:
+                    if match_word(w, preamble['wRDA']):
+                        r = (r << 12) + (w & 0x0FFF)
+                        pos += 12
+                    elif match_word(w, preamble['wCON']):
+                        r = (r << 15) + (w & 0x7FFF)
+                        pos += 15
 
-            # extract information for different cases
-            if w.startswith(preamble['wEOM']):
-                self.num_data = int(w[4:10], 2)
-                if self.data is not None: # discard last 0 value
-                    self.data = self.data[:self.num_data]
-                try:
-                    self.hit_type = hittype_str[w[10:12]]
-                    self.stop_type = endofmessage_str[w[13:16]]
-                except KeyError:
-                    self._valid = False
+                while pos >= 0:
+                    mask = 0x1FF << pos
+                    yield (r & mask) >> pos
+                    r = (r & ~mask)
+                    pos -= 9
 
-            elif w.startswith(preamble['wBOM']):
-                self.buffer_overflow_count = int(w[8:16], 2)
+        data = [x-512 if x > 255 else x
+                for x in iter_data()]
+        self._data = data[:self.num_data]
 
-            elif w.startswith(preamble['wEPM']):
-                self.epoch_count = int(w[4:16], 2)
 
-        #------------------------------------------------------------
-        # get info from info words
-        #------------------------------------------------------------
-        info_words = [word for word in message
-                      if word.startswith(preamble['wINF'])]
-        
-        if info_words:
-            w = info_words[0]
-            try:
-                self.info_type = infotype_str[w[4:8]]
-            except KeyError:
-                self._valid = False
-            # channel_id only for some info types
-            if w[4:8] in [infotype[t] for t in ['iDIS', 'iNGT']]:
-                self.channel_id = int(word[8:12], 2)
+    def data(self):
+        """Get the data samples."""
+        if self._data is None:
+            self._update_data()
+        return self._data
+                
 
-        # end of __init__()
-
-    #----------------------------------------------------------------
-    # make human-readable report of a message
-    #----------------------------------------------------------------
     def report(self, verbose=False):
+        """Make a human-readable report."""
+        self._update_data()
         s = []
+
         if verbose:
-            s.append('\n'.join('%3i: %s' % (i, word)
+            s.append('\n'.join('%3i: %04X' % (i, word)
                                for (i, word) in enumerate(self.words)))
 
         if self.group_id is not None:
@@ -256,36 +231,36 @@ class Message():
             s.append('channel: %i' % self.channel_id)
         if self.timestamp is not None:
             s.append('timestamp: %i' % self.timestamp)
-        if self.data is not None:
+        if self._data:
             s.append('data (%i values): ' % self.num_data +
-                     ', '.join(str(x) for x in self.data))
+                     ', '.join(str(x) for x in self._data))
         if self.hit_type is not None:
-            s.append('hit type: %s' % self.hit_type)
+            s.append('hit type: %s' % hittype_str[self.hit_type])
         if self.stop_type is not None:
-            s.append('stop type: %s' % self.stop_type)
+            s.append('stop type: %s' % stoptype_str[self.stop_type])
         if self.buffer_overflow_count is not None:
             s.append('buffer overflow count: %i' % self.buffer_overflow_count)
         if self.epoch_count is not None:
             s.append('epoch count: %i' % self.epoch_count)
         if self.info_type is not None:
-            s.append('info type: %s' % self.info_type)
+            s.append('info type: %s' % infotype_str[self.info_type])
 
         return '\n'.join(s)
 
-    #----------------------------------------------------------------
-    # 'print'ing a message returns verbose report
-    #----------------------------------------------------------------
+
     def __str__(self):
         return self.report(verbose=True)
 
-    #----------------------------------------------------------------
-    # return data points in a format suitable for gnuplot (x: time in ns)
-    #----------------------------------------------------------------
-    def data_gnuplot(self, mask='1'*32, T=40):
-        if self.data is not None:
-            t = [i*T for (i, m) in enumerate(mask) if m=='1']
-            return'\n'.join('%5i %5i' % (x, y)
-                            for (x, y) in zip(t, self.data))
+
+    # TODO move this somewhere else
+    ##----------------------------------------------------------------
+    ## return data points in a format suitable for gnuplot (x: time in ns)
+    ##----------------------------------------------------------------
+    #def data_gnuplot(self, mask='1'*32, T=40):
+    #    if self.data is not None:
+    #        t = [i*T for (i, m) in enumerate(mask) if m=='1']
+    #        return'\n'.join('%5i %5i' % (x, y)
+    #                        for (x, y) in zip(t, self.data))
 
 
 
