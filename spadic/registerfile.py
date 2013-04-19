10da739f8e1283e7f2adf43a2343eaa93b81fc49
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-04-19 18:58:08 +0200

    made RegisterFile class generic and added SpadicRegisterFile as subclass

--- spadic/registerfile.py
+++ spadic/registerfile.py
@@ -1,3 +1,7 @@
+#====================================================================
+# generic representation of a single register and a register file
+#====================================================================
+
 class Register:
     """Representation of a single hardware register.
     
@@ -78,68 +82,16 @@ class Register:
         return self._cache
 
 
-#--------------------------------------------------------------------
-# map of user accessible registers in the SPADIC register file
-#--------------------------------------------------------------------
-RF_MAP = {                  # address, size
-   "overrides"               : (  0x8,  7),
-   "CbmNetAddr"              : ( 0x10, 16),
-   "EpochCounter"            : ( 0x18, 16),
-   "threshold1"              : ( 0x20,  9),
-   "threshold2"              : ( 0x28,  9),
-   "compDiffMode"            : ( 0x30,  1),
-   "hitWindowLength"         : ( 0x38,  6),
-   "selectMask_l"            : ( 0x40, 16),
-   "selectMask_h"            : ( 0x48, 16),
-   "bypassFilterStage"       : ( 0x50,  5),
-   "aCoeffFilter_l"          : ( 0x58, 16),
-   "aCoeffFilter_h"          : ( 0x60,  2),
-   "bCoeffFilter_l"          : ( 0x68, 16),
-   "bCoeffFilter_h"          : ( 0x70,  8),
-   "scalingFilter"           : ( 0x78,  9),
-   "offsetFilter"            : ( 0x80,  9),
-   "groupIdA"                : ( 0x88,  8),
-   "groupIdB"                : ( 0x90,  8),
-#  "neighborSelectMatrixA_0" : ( 0x98, 16), # generated below
-#  "neighborSelectMatrixA_1" : ( 0xA0, 16),
-#  ...
-#  "neighborSelectMatrixA_30": (0x188,  4),
-#  "neighborSelectMatrixB_0" : (0x190, 16),
-#  ...
-#  "neighborSelectMatrixB_30": (0x280,  4),
-   "disableChannelA"         : (0x288, 16),
-   "disableChannelB"         : (0x290, 16),
-   "disableEpochChannelA"    : (0x298,  1),
-   "disableEpochChannelB"    : (0x2a0,  1),
-   "enableTestOutput"        : (0x2a8,  1),
-   "testOutputSelGroup"      : (0x2b0,  1),
-   "enableTestInput"         : (0x2b8,  1),
-   "enableAdcDec_l"          : (0x2c0, 16), # multiplex ADC signals to decoupling
-   "enableAdcDec_h"          : (0x2c8,  5),
-   "triggerMaskA"            : (0x2d0, 16), # enables DLM11 to trigger the hit logic
-   "triggerMaskB"            : (0x2d8, 16),
-   "enableAnalogTrigger"     : (0x2e0,  1), # enables DLM12 to inject an analog pulse into ch. 31
-   "enableTriggerOutput"     : (0x2e8,  1), # enables DLM10 to generate the "TriggerOut" signal
-}
-  
-# neighborSelectMatrix registers are generated here
-for (group, base_addr) in [('A', 0x98), ('B', 0x190)]:
-    for i in range(31): # 30*16 + 4 = 484
-        name = 'neighborSelectMatrix%s_%i' % (group, i)
-        addr = base_addr + 8*i
-        RF_MAP[name] = (addr, (4 if i==30 else 16))
-
-
 class RegisterFile:
-    """Representation of the SPADIC register file."""
+    """Representation of a generic register file."""
 
-    def __init__(self, spadic):
+    def __init__(self, reg_map, write_method, read_method):
         """Set up all registers."""
         self._registers = {}
-        for (name, (addr, size)) in RF_MAP.iteritems():
+        for (name, (addr, size)) in reg_map.iteritems():
             r = Register(addr, size)
-            r._write = spadic.write_register
-            r._read = spadic.read_register
+            r._write = write_method
+            r._read = read_method
             self._registers[name] = r
 
         # emulate dictionary behaviour (read-only)
@@ -185,4 +137,71 @@ class RegisterFile:
         for name in self:
             config[name] = self[name].read()
         return config
+
+
+#====================================================================
+# representation of the SPADIC 1.0 register file
+#====================================================================
+
+#--------------------------------------------------------------------
+# map of the user accessible registers
+#--------------------------------------------------------------------
+SPADIC_RF = {               # address, size
+   "overrides"               : (  0x8,  7),
+   "CbmNetAddr"              : ( 0x10, 16),
+   "EpochCounter"            : ( 0x18, 16),
+   "threshold1"              : ( 0x20,  9),
+   "threshold2"              : ( 0x28,  9),
+   "compDiffMode"            : ( 0x30,  1),
+   "hitWindowLength"         : ( 0x38,  6),
+   "selectMask_l"            : ( 0x40, 16),
+   "selectMask_h"            : ( 0x48, 16),
+   "bypassFilterStage"       : ( 0x50,  5),
+   "aCoeffFilter_l"          : ( 0x58, 16),
+   "aCoeffFilter_h"          : ( 0x60,  2),
+   "bCoeffFilter_l"          : ( 0x68, 16),
+   "bCoeffFilter_h"          : ( 0x70,  8),
+   "scalingFilter"           : ( 0x78,  9),
+   "offsetFilter"            : ( 0x80,  9),
+   "groupIdA"                : ( 0x88,  8),
+   "groupIdB"                : ( 0x90,  8),
+#  "neighborSelectMatrixA_0" : ( 0x98, 16), # generated below
+#  "neighborSelectMatrixA_1" : ( 0xA0, 16),
+#  ...
+#  "neighborSelectMatrixA_30": (0x188,  4),
+#  "neighborSelectMatrixB_0" : (0x190, 16),
+#  ...
+#  "neighborSelectMatrixB_30": (0x280,  4),
+   "disableChannelA"         : (0x288, 16),
+   "disableChannelB"         : (0x290, 16),
+   "disableEpochChannelA"    : (0x298,  1),
+   "disableEpochChannelB"    : (0x2a0,  1),
+   "enableTestOutput"        : (0x2a8,  1),
+   "testOutputSelGroup"      : (0x2b0,  1),
+   "enableTestInput"         : (0x2b8,  1),
+   "enableAdcDec_l"          : (0x2c0, 16), # multiplex ADC signals to decoupling
+   "enableAdcDec_h"          : (0x2c8,  5),
+   "triggerMaskA"            : (0x2d0, 16), # enables DLM11 to trigger the hit logic
+   "triggerMaskB"            : (0x2d8, 16),
+   "enableAnalogTrigger"     : (0x2e0,  1), # enables DLM12 to inject an analog pulse into ch. 31
+   "enableTriggerOutput"     : (0x2e8,  1), # enables DLM10 to generate the "TriggerOut" signal
+}
+  
+# neighborSelectMatrix registers are generated here
+for (group, base_addr) in [('A', 0x98), ('B', 0x190)]:
+    for i in range(31): # 30*16 + 4 = 484
+        name = 'neighborSelectMatrix%s_%i' % (group, i)
+        addr = base_addr + 8*i
+        SPADIC_RF[name] = (addr, (4 if i==30 else 16))
+
+
+#--------------------------------------------------------------------
+# inherit the generic register file class
+#--------------------------------------------------------------------
+class SpadicRegisterFile(RegisterFile):
+    """Representation of the SPADIC register file."""
+    def __init__(self, spadic):
+        wr = spadic.write_register
+        rd = spadic.read_register
+        RegisterFile.__init__(self, SPADIC_RF, wr, rd)
 
