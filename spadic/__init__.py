c82a1e2c7b3db438b334d35d1c3ead0a9a0fbb05
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-04-22 13:54:46 +0200

    temporarily excluded Controller from __init__

--- spadic/__init__.py
+++ spadic/__init__.py
@@ -1,12 +1,12 @@
 from basic import Spadic
 from message import Message
-from control import Controller
+#from control import Controller
 
 #del basic, message, control
 
 __version__ = '0.9.6'
 
 __all__ = [
-    'Spadic', 'Message', 'Controller'
+    'Spadic', 'Message'#, 'Controller'
 ]
 
