608ce16dd3a3871909f93117c01ab0587e3cfeb6
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-06-14 18:44:30 +0200

    added --load option to spadic_ui script

--- spadic_ui.py
+++ spadic_ui.py
@@ -3,11 +3,26 @@
 import sys
 import spadic
 
+#--------------------------------------------------------------------
+# parse options
+#--------------------------------------------------------------------
 reset = "--reset" in sys.argv
-try:
-    s = spadic.Spadic(reset, _debug_cbmif=1)
-except:
-    sys.exit(sys.exc_value)
+full_error = "--full-error" in sys.argv
 
-s.ui_run()
+try:
+    load_file = sys.argv[sys.argv.index("--load")+1]
+except:
+    load_file = None
+
+#--------------------------------------------------------------------
+# start spadic controller and user interface
+#--------------------------------------------------------------------
+try:
+    s = spadic.Spadic(reset, load_file, _debug_cbmif=1)
+    s.ui_run()
+except:
+    if full_error:
+        raise
+    else:
+        sys.exit(sys.exc_value)
 
