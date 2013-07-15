2b22bb9c4cef43ee78d01e6d6a6b32184936a229
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-07-15 14:01:16 +0200

    moved global baseline trim from frontend (renamed to CSA bias) to ADC bias settings; adjusted user interface accordingly; version -> 1.0.7

--- spadic/control/adcbias.py
+++ spadic/control/adcbias.py
@@ -6,6 +6,7 @@ _ADC_VPLOADFB = 0
 _ADC_VPLOADFB2 = 0
 _ADC_VPFB = 0
 _ADC_VPAMP = 0
+_ADC_BASELINE = 0
 
 class AdcBias(ControlUnitBase):
     """Controls the ADC bias settings."""
@@ -15,11 +16,14 @@ class AdcBias(ControlUnitBase):
 
     def reset(self):
         self.set(_ADC_VNDEL, _ADC_VPDEL, _ADC_VPLOADFB, _ADC_VPLOADFB2,
-                 _ADC_VPFB, _ADC_VPAMP)
+                 _ADC_VPFB, _ADC_VPAMP, _ADC_BASELINE)
 
     def set(self, vndel=None, vpdel=None, vploadfb=None,
-                  vploadfb2=None, vpfb=None, vpamp=None):
-        """Set VNDel, VPDel, VPLoadFB, VPLoadFB2, VPFB, VPAmp values."""
+                  vploadfb2=None, vpfb=None, vpamp=None, baseline=None):
+        """
+        Set VNDel, VPDel, VPLoadFB, VPLoadFB2, VPFB, VPAmp, and
+        baselineTrimN values.
+        """
         if vndel is not None:
             checkvalue(vndel, 0, 127, 'VNDel')
             self._vndel = vndel
@@ -38,6 +42,9 @@ class AdcBias(ControlUnitBase):
         if vpamp is not None:
             checkvalue(vpamp, 0, 127, 'VPAmp')
             self._vpamp = vpamp
+        if baseline is not None:
+            checkvalue(baseline, 0, 127, 'baseline')
+            self._baseline = baseline
 
         self._shiftregister['VNDel'].set(self._vndel)
         self._shiftregister['VPDel'].set(self._vpdel)
@@ -45,6 +52,7 @@ class AdcBias(ControlUnitBase):
         self._shiftregister['VPLoadFB2'].set(self._vploadfb2)
         self._shiftregister['VPFB'].set(self._vpfb)
         self._shiftregister['VPAmp'].set(self._vpamp)
+        self._shiftregister['baselineTrimN'].set(self._baseline)
 
     def apply(self):
         self._shiftregister.apply()
@@ -57,6 +65,7 @@ class AdcBias(ControlUnitBase):
         self._vploadfb2 = self._shiftregister['VPLoadFB2'].get()
         self._vpfb = self._shiftregister['VPFB'].get()
         self._vpamp = self._shiftregister['VPAmp'].get()
+        self._baseline = self._shiftregister['baselineTrimN'].get()
 
     def get(self):
         return {'vndel': self._vndel,
@@ -64,11 +73,13 @@ class AdcBias(ControlUnitBase):
                 'vploadfb': self._vploadfb,
                 'vploadfb2': self._vploadfb2,
                 'vpfb': self._vpfb,
-                'vpamp': self._vpamp}
+                'vpamp': self._vpamp,
+                'baseline': self._baseline}
 
     def __str__(self):
         return ('VNDel: %3i  VPDel: %3i  VPLoadFB: %3i\n'
-                'VPLoadFB2: %3i  VPFB: %3i  VPAmp: %3i' %
+                'VPLoadFB2: %3i  VPFB: %3i  VPAmp: %3i\n'
+                'baseline: %3i' %
                 (self._vndel, self._vpdel, self._vploadfb,
-                 self._vploadfb2, self._vpfb, self._vpamp))
+                 self._vploadfb2, self._vpfb, self._vpamp, self._baseline))
 
--- spadic/control/ui/adcbias.py
+++ spadic/control/ui/adcbias.py
@@ -7,7 +7,7 @@ class AdcBiasFrame(mutti.Frame):
         self.control_panels = []
         self._log = _log
 
-        grid = mutti.Grid(2, 3)
+        grid = mutti.Grid(3, 3)
 
         u = spadic_controller.adcbias
         for (i, d) in enumerate([
@@ -15,19 +15,21 @@ class AdcBiasFrame(mutti.Frame):
                      min_width=13),                                
           SpadicDial(u, "vpdel", (0, 127), 3, "VPDel",
                      min_width=13),                                
-          SpadicDial(u, "vploadfb", (0, 127), 3, "  VPLoadFB",
-                     min_width=16),                                
+          SpadicDial(u, "vploadfb", (0, 127), 3, "VPLoadFB",
+                     min_width=13),                                
           SpadicDial(u, "vploadfb2", (0, 127), 3, "  VPLoadFB2",
                      min_width=16),                                
           SpadicDial(u, "vpfb", (0, 127), 3, "  VPFB",
-                     min_width=14),                                
+                     min_width=16),                                
           SpadicDial(u, "vpamp", (0, 127), 3, "  VPAmp",
-                     min_width=14),                                
+                     min_width=16),                                
+          SpadicDial(u, "baseline", (0, 127), 3, "  Baseline",
+                     min_width=15),
           ]):
             d._status = statusbar
             d._log = _log
             self.control_panels.append(d)
-            grid.adopt(d, row=(i%2), col=(i//2))
+            grid.adopt(d, row=(i%3), col=(i//3))
 
         self.adopt(grid)
 
