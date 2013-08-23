050b723da2facd3c96a34868038248e3884a9b3d
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-08-23 16:59:44 +0200

    the spadic package can now be imported even if libFTDI is not installed. this way, the Spadic(...)Clients can still be used, only the Spadic and SpadicServer classes will not be available

--- spadic/__init__.py
+++ spadic/__init__.py
@@ -1,11 +1,24 @@
-from main import Spadic
-from server import SpadicServer
+__all__ = []
+
+# Try to move Spadic and SpadicServer from their module namespaces to the
+# top-level spadic package namespace (i.e., here). If libFTDI is not
+# installed, this will fail, but we allow the spadic package to be
+# imported without them.
+try:
+    from main import Spadic
+    from server import SpadicServer
+    del main
+    del server
+    __all__ += ['Spadic', 'SpadicServer']
+except ImportError:
+    pass
+
+# Move the clients from their module namespace to the top-level spadic
+# package namespace. They don't need libFTDI and will be available in any
+# case.
 from client import SpadicControlClient, SpadicDataClient
-del main
-del server
 del client
+__all__ += ['SpadicControlClient', 'SpadicDataClient']
 
 __version__ = '1.1.4'
-__all__ = ['Spadic', 'SpadicServer',
-           'SpadicControlClient', 'SpadicDataClient']
 
