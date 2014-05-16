bd02d095a50a614c3b66e7536420016e565ee56b
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-05-16 17:24:33 +0200

    improved test script

--- ext/test_message.py
+++ ext/test_message.py
@@ -1,10 +1,33 @@
 import ctypes
+import struct
 
 message = ctypes.cdll.LoadLibrary("libmessage.so")
-words = ctypes.c_uint16 * 8
 
-w = words(*[1, 0x80ab, 0x9123, 4, 5, 0x8123, 7, 8])
+def data_from_bytes(b):
+    fmt = '>%iH'%(len(b)/2)
+    return struct.unpack(fmt, b)
 
-count = message.seek_message_start_all_wrap(w, 8)
-print "result:", count
+def data_from_string(s):
+    return [int(x, 16) for x in s.split()]
+
+def read_messages(data):
+    n = len(data)
+    t = ctypes.c_ushort*n
+    w = t(*data)
+    count = message.seek_message_start_all_wrap(w, n)
+    print "result:", count
+
+
+if __name__=='__main__':
+    s = """
+    8123
+    9456
+    A789
+    1234
+    2345
+    3456
+    BABC
+    """
+    d = data_from_string(s)
+    read_messages(d)
 
