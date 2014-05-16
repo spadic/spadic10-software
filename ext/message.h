57f985fd3be6d22cf0c829851d6064f60434b5d3
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-05-16 11:34:38 +0200

    added #ifdef __cplusplus extern "C"

--- ext/message.h
+++ ext/message.h
@@ -1,5 +1,8 @@
 #ifndef SPADIC_MESSAGE_H
 #define SPADIC_MESSAGE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stdint.h>
 
@@ -63,4 +66,7 @@ uint16_t* seek_message_start(uint16_t* begin, uint16_t* end);
 size_t seek_message_start_all(uint16_t* begin, uint16_t* end);
 size_t seek_message_start_all_wrap(uint16_t* begin, size_t length);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
