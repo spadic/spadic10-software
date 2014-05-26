921e7aaa172ba3e614c5c8e76cb56d1e5a8099db
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-05-26 14:24:03 +0200

    fixed programming error: [] has higher precedence than -> and *, need parentheses

--- ext/message.c
+++ ext/message.c
@@ -97,7 +97,7 @@ static void fill_wRDA(Message *m, uint16_t w)
     m->raw_buf = malloc(sizeof *m->raw_buf);
     if (!m->raw_buf) { return; }
     m->raw_count = 0;
-    *m->raw_buf[m->raw_count++] = w & 0x0FFF;
+    (*m->raw_buf)[m->raw_count++] = w & 0x0FFF;
 }
 
 static void fill_wEOM(Message *m, uint16_t w)
@@ -136,7 +136,7 @@ static void fill_wCON(Message *m, uint16_t w)
 {
     if (!m->raw_buf) { return; }
     if (m->raw_count >= MAX_RAW_COUNT) { return; }
-    *m->raw_buf[m->raw_count++] = w & 0x7FFF;
+    (*m->raw_buf)[m->raw_count++] = w & 0x7FFF;
 }
 
 /*==== public functions ===========================================*/
