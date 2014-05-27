3707fd866a9c12f3dcd5b3299d9d63fe92bf927b
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-05-27 16:45:54 +0200

    added more documentation, added "member groups" for doxygen

--- ext/message.h
+++ ext/message.h
@@ -20,7 +20,8 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
-/* create and destroy message objects */
+/** \name Create, fill and destroy message objects */
+/**@{*/
 typedef struct _message Message;
 /**<
  * Represents SPADIC messages.
@@ -35,8 +36,7 @@ void message_delete(Message *m);
  * Destroy a message object.
  * Nothing is done if `m` is `NULL`.
  */
-size_t message_read_from_buffer(Message *m,
-                                const uint16_t *buf, size_t len);
+size_t message_read_from_buffer(Message *m, const uint16_t *buf, size_t len);
 /**<
  * Read words from `buf` and fill message `m`.
  *
@@ -65,7 +65,7 @@ size_t message_read_from_buffer(Message *m,
  *
  * It is not guaranteed that a complete message was contained in the
  * consumed words (cases b-d), this can be checked afterwards using
- * `message_is_complete(m)`.
+ * message_is_complete().
  *
  * The passed message object `m` is reset if, and only if, a
  * start-of-message word is encountered. This means
@@ -93,15 +93,17 @@ size_t message_read_from_buffer(Message *m,
  * }
  * \endcode
  */
+/**@}*/
 
-/* query message type and completeness */
+/** \name Query message type and status */
+/**@{*/
 int message_is_hit(const Message *m);
 /**<
  * \return Non-zero if `m` is a regular hit message.
  *
  * Indicates that the following data is available:
  * - group ID (message_get_group_id())
- * - channel ID (message_get_channel())
+ * - channel ID (message_get_channel_id())
  * - timestamp (message_get_timestamp())
  * - number of samples (message_get_num_samples()),
  *   and the samples themselves (message_get_samples())
@@ -123,7 +125,7 @@ int message_is_buffer_overflow(const Message *m);
  *
  * Indicates that the following data is available:
  * - group ID (message_get_group_id())
- * - channel ID (message_get_channel())
+ * - channel ID (message_get_channel_id())
  * - timestamp (message_get_timestamp())
  * - number of lost hits (message_get_buffer_overflow_count()),
  */
@@ -162,18 +164,49 @@ int message_is_valid(const Message *m);
  * - info message
  */
 int message_is_complete(const Message *m);
+/**<
+ * \return Non-zero if `m` is a complete message.
+ *
+ * A message is considered "complete" when both the start and the end of
+ * the message have been encountered. Use this function to determine
+ * whether message_read_from_buffer() can further fill a given message
+ * object.
+ *
+ * Note that this is different from message_is_valid(): a message can be
+ * complete and not valid, but a valid message is always complete.
+ */
+/**@}*/
 
-/* access message data */
+/** \name Access message data */
+/**@{*/
 uint8_t message_get_group_id(const Message *m);
+/**< \return The group ID, if available, undefined otherwise. */
 uint8_t message_get_channel_id(const Message *m);
+/**< \return The channel ID, if available, undefined otherwise. */
 uint16_t message_get_timestamp(const Message *m);
+/**< \return The timestamp, if available, undefined otherwise. */
 int16_t *message_get_samples(const Message *m);
+/**<
+ * \return Pointer to a memory location containing the samples, if
+ * available, `NULL` otherwise.
+ *
+ * Determine the number of available samples using
+ * message_get_num_samples(). The memory containing the samples is
+ * reclaimed when `m` is destroyed using message_delete().
+ */
 uint8_t message_get_num_samples(const Message *m);
+/**< \return The number of samples, if available, undefined otherwise. */
 uint8_t message_get_hit_type(const Message *m);
+/**< \return The hit type, if available, undefined otherwise. */
 uint8_t message_get_stop_type(const Message *m);
+/**< \return The stop type, if available, undefined otherwise. */
 uint8_t message_get_buffer_overflow_count(const Message *m);
+/**< \return The buffer overflow count, if available, undefined otherwise. */
 uint16_t message_get_epoch_count(const Message *m);
+/**< \return The epoch count, if available, undefined otherwise. */
 uint8_t message_get_info_type(const Message *m);
+/**< \return The info type, if available, undefined otherwise. */
+/**@}*/
 
 #ifdef __cplusplus
 }
