c9528472e57600f4088a3401dedb57f4c0d4d705
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-04 16:56:38 +0200

    renamed local variable in unpack_raw() x->v, to avoid confusion with 0x prefix of hex literals

--- ext/message.c
+++ ext/message.c
@@ -169,15 +169,15 @@ size_t raw_count(size_t num_samples)
 void unpack_raw(uint16_t *raw, int16_t *samples, size_t num_samples)
 {
     const uint16_t mask = 0x1FF;
-    uint16_t x;
+    uint16_t v;
     uint32_t r = 0;
     size_t n = 0;
 
     int pos;
     for (pos = -12; ; pos += 15) {
         for (; pos >= 0; pos -= 9) {
-            x = (r >> pos) & mask;
-            samples[n++] = (x >= 0x100 ? x-0x200 : x); /* 2's complement */
+            v = (r >> pos) & mask;
+            samples[n++] = (v >= 0x100 ? v-0x200 : v); /* 2's complement */
             if (n >= num_samples) { return; }
             r &= ~(mask << pos);
         }
