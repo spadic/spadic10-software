8545ab3ff78dc056ca4cfd8047bd0c009e683571
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-04 18:47:33 +0200

    moved stop/info/hit types to header file, rewrote as enum, adjusted formatting of documentation to automatically generate links

--- ext/message.h
+++ ext/message.h
@@ -19,6 +19,56 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * Stop types as returned by message_get_stop_type().
+ */
+enum stop_types {
+    sEND = 0x0, /**< Normal end of message */
+    sEBF = 0x1, /**< Channel buffer full */
+    sEFF = 0x2, /**< Ordering FIFO full */
+    sEDH = 0x3, /**< Multi hit */
+    sEDB = 0x4, /**< Multi hit and channel buffer full */
+    sEDO = 0x5, /**< Multi hit and ordering FIFO full */
+/* unused
+    sXX1 = 0x6,
+    sXX2 = 0x7,
+*/
+};
+
+/**
+ * Info types as returned by message_get_info_type().
+ */
+enum info_types {
+    iDIS = 0x0, /**< Channel disabled during message building */
+    iNGT = 0x1, /**< Next grant timeout */
+    iNRT = 0x2, /**< Next request timeout */
+    iNBE = 0x3, /**< New grant but channel empty */
+    iMSB = 0x4, /**< Corruption in message builder */
+    iNOP = 0x5, /**< Empty word */
+    iSYN = 0x6, /**< Epoch out of sync */
+/* unused
+    iXX3 = 0x7,
+    iXX4 = 0x8,
+    iXX5 = 0x9,
+    iXX6 = 0xA,
+    iXX7 = 0xB,
+    iXX8 = 0xC,
+    iXX9 = 0xD,
+    iXXA = 0xE,
+    iXXB = 0xF,
+*/
+};
+
+/**
+ * Hit types as returned by message_get_hit_type().
+ */
+enum hit_types {
+    hGLB = 0x0, /**< Global trigger */
+    hSLF = 0x1, /**< Self triggered */
+    hNBR = 0x2, /**< Neighbor triggered */
+    hSAN = 0x3, /**< Self and neighbor triggered */
+};
+
 /**@{
  * \name Create, fill and destroy message objects
  */
@@ -168,7 +218,7 @@ int message_is_hit_aborted(const Message *m);
  *
  * Indicates that the following data is available:
  * - channel ID (message_get_channel_id())
- * - info type (message_get_info_type()), can be `DIS` or `MSB`
+ * - info type (message_get_info_type()), can be #iDIS or #iMSB
  */
 int message_is_buffer_overflow(const Message *m);
 /**<
@@ -195,15 +245,15 @@ int message_is_epoch_out_of_sync(const Message *m);
  * Indicates that the following data is available:
  * - group ID (message_get_group_id())
  * - least significant 8 bits of epoch count (message_get_epoch_count())
- * - info type (can only be `SYN`, so no need to check it)
+ * - info type (can only be #iSYN, so no need to check it)
  */
 int message_is_info(const Message *m);
 /**<
  * \return Non-zero if `m` is an info message
  *
  * Indicates that the following data is available:
- * - info type (message_get_info_type()), can be `NGT`, `NRT`, or `NBE`
- * - channel ID (message_get_channel_id()), if the info type is `NGT` or `NBE`
+ * - info type (message_get_info_type()), can be #iNGT, #iNRT, or #iNBE
+ * - channel ID (message_get_channel_id()), if the info type is #iNGT or #iNBE
  */
 /**@}*/
 
@@ -231,15 +281,15 @@ int16_t *message_get_samples(const Message *m);
 uint8_t message_get_num_samples(const Message *m);
 /**< \return The number of samples, if available, undefined otherwise. */
 uint8_t message_get_hit_type(const Message *m);
-/**< \return The hit type, if available, undefined otherwise. */
+/**< \return One of the #hit_types, if available, undefined otherwise. */
 uint8_t message_get_stop_type(const Message *m);
-/**< \return The stop type, if available, undefined otherwise. */
+/**< \return One of the #stop_types, if available, undefined otherwise. */
 uint8_t message_get_buffer_overflow_count(const Message *m);
 /**< \return The buffer overflow count, if available, undefined otherwise. */
 uint16_t message_get_epoch_count(const Message *m);
 /**< \return The epoch count, if available, undefined otherwise. */
 uint8_t message_get_info_type(const Message *m);
-/**< \return The info type, if available, undefined otherwise. */
+/**< \return One of the #info_types, if available, undefined otherwise. */
 /**@}*/
 
 #ifdef __cplusplus
