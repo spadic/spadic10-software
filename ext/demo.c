ff0298f786b8740518311cea922d4bd870a89227
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-05 12:51:49 +0200

    included demo.c in documentation

--- ext/demo.c
+++ ext/demo.c
@@ -1,24 +1,53 @@
+/**
+ * \example demo.c
+ * This example shows the basic usage of the SPADIC message library.
+ *
+ * An array of 16-bit values is created, which represents the data coming
+ * from the chip. It contains two hit messages, where one word of the
+ * second message is deliberately commented out, so that the raw data
+ * containing the samples is invalid.
+ *
+ * The output should look like this:
+ *
+ *     message is hit
+ *     group ID: 1
+ *     channel ID: 2
+ *     timestamp: 1638
+ *     num. samples: 7
+ *     hit type: 1
+ *     stop type: 0
+ *     samples: 1 2 3 4 5 6 7
+ *
+ *     message is hit
+ *     group ID: 3
+ *     channel ID: 4
+ *     timestamp: 2184
+ *     num. samples: 7
+ *     hit type: 1
+ *     stop type: 0
+ *     raw data invalid
+ */
 #include <stdio.h>
 #include "message.h"
 
 #define N 15
 const uint16_t buf[N] = {
-    0x8012,
+    0x8012, /* start of message 1 */
     0x9666,
     0xA008,
     0x0403,
     0x0100,
     0x5030,
     0x0E00,
-    0xB1D0,
-    0x8034,
+    0xB1D0, /* end of message 1 */
+    0x8034, /* start of message 2 */
     0x9888,
     0xA008,
 /*  0x0403, missing -> raw data invalid */
     0x0100,
     0x5030,
     0x0E00,
-    0xB1D0
+    0xB1D0, /* end of message 2 */
 };
 
 void print_hit_message(Message *m)
@@ -45,22 +74,16 @@ void print_hit_message(Message *m)
 int main(void)
 {
     Message *m = message_new();
-
     size_t n = 0;
-
     while (n < N) {
         n += message_read_from_buffer(m, buf+n, N-n);
-
         if (!message_is_complete(m)) {
             printf("\nincomplete message\n");
-            return 1;
         }
-
         if (message_is_hit(m)) {
             printf("\nmessage is hit\n");
             print_hit_message(m);
         }
     }
-
     return 0;
 }
--- ext/doc/mainpage.md
+++ ext/doc/mainpage.md
@@ -5,4 +5,6 @@
 This library provides functions to extract SPADIC messages from captured
 raw data and to access the message content.
 
-You want to look at message.h.
+You want to look at message.h for a detailed description of the API.
+
+An example of the basic usage can be found in \ref demo.c.
