62b8bce6d61d753c354fe1056df5b419ffeb6b59
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-08 12:04:01 +0200

    changed message_iter() to class MessageIterator (with persistence between calls)

--- lib/message/test/message_iter.py
+++ lib/message/test/message_iter.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 
-def message_iter(words):
-    buf = words
-    m = Message()
-    while buf:
-        n = m.read_from_buffer(buf)
-        if not n:
-            break
-        buf = buf[n:]
-        if not m.is_complete():
-            continue
-        yield m
-        m = Message()
 
+class MessageIterator:
+    def __init__(self):
+        self.buf = []
+        self.m = None
+
+    def __call__(self, words):
+        buf = self.buf + words
+        m = self.m or Message()
+        while buf:
+            n = m.read_from_buffer(buf)
+            if not m.is_complete():
+                break
+            yield m
+            buf = buf[n:]
+            m = Message()
+        self.buf = buf
+        self.m = m
 if __name__=='__main__':
     import sys
+    mi = MessageIterator()
     words = [int(x, 16) for x in sys.stdin
              if x.strip() and not x.startswith('#')]
-    for (i, m) in enumerate(message_iter(words)):
+    for (i, m) in enumerate(mi(words)):
         print i, m
