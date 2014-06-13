0cbdf596857e99b766bfbf4e4b0c600cc4158d06
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-13 18:01:15 +0200

    adjusted header of message_reader to match the demo program

--- lib/message/message_reader.h
+++ lib/message/message_reader.h
@@ -4,6 +4,10 @@
  *
  * This is the API for the SPADIC 1.0 Message Library.
  *
+ * All functions receiving a pointer `r` to a MessageReader object assume
+ * that it has been properly allocated and initialized (by
+ * message_reader_new()).
+ *
  * **DRAFT**
  */
 
@@ -24,52 +28,41 @@ MessageReader message_reader_new(void);
 void message_reader_delete(MessageReader *r);
 /**<
  * Clean up and deallocate a message reader.
- *
- * Nothing is done if `r` is NULL.
  */
 void message_reader_reset(MessageReader *r);
 /**<
  * Reset a message reader to its initial state.
- *
- * TODO which one?
- * - Nothing is done if `r` is NULL.
- * - `r` must point to a properly allocated and initialized message reader.
  */
-size_t message_reader_add_buffer(MessageReader *r,
-                                 const uint16_t *buf, size_t len);
+void message_reader_add_buffer(MessageReader *r,
+                               const uint16_t *buf, size_t len);
 /**<
- * Point the message reader to a new buffer with `len` words.
+ * Add a new buffer with `len` words to a message reader.
  *
- * Nothing is done if `buf` is NULL.
- *
- * TODO which one?
- * - Nothing is done if `r` is NULL.
- * - `r` must point to a properly allocated and initialized message reader.
- *
- * Attempts to allocate new messages that are subsequently filled.
- *
- * \return Number of currently allocated messages. If this is zero, `buf`
- * was not added.
+ * Nothing is done if `buf` is NULL or `len` is zero.
  */
-Message *message_reader_next_message(MessageReader *r);
+Message *message_reader_get_message(MessageReader *r);
 /**<
  * Read the next message.
  *
- * \return Pointer to a message object, NULL if all buffers are depleted,
- * undefined if called more often than indicated by
- * message_reader_add_buffer().
+ * \return Pointer to a message object, if available, `NULL` otherwise.
  *
- * what if no new message object could be created? (message_new() failed)
+ * Normally, `NULL` is returned because all buffers are depleted. Check
+ * this with message_reader_is_empty().
  *
- * - message will be complete
- * - must check message_is_valid()
- * - incomplete message will be kept
+ * The returned messages are always complete (message_is_complete()).
  */
-const uint16_t *message_reader_next_depleted_buffer(MessageReader *r);
+int message_reader_is_empty(MessageReader *r);
+/**<
+ * \return Non-zero if all buffers are depleted.
+ *
+ * Add more buffers to read from with message_reader_add_buffer().
+ *
+ * If message_reader_get_message() returned `NULL` and the reader is not
+ * empty (this function returns zero), an internal error has occurred.
+ */
+const uint16_t *message_reader_get_depleted(MessageReader *r);
 /**<
  * \return Next depleted buffer, NULL if no depleted buffers are left.
- *
- * Nothing is done if `r` is `NULL`.
  */
 
 #endif
