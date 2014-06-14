1861a0d83b5d50f42a1616d94b410beb0e9bb986
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-14 14:27:16 +0200

    continued (very rough) implementation of message_reader

--- lib/message/message_reader.c
+++ lib/message/message_reader.c
@@ -1,8 +1,8 @@
 Message *message_reader_next_message(MessageReader *r)
 {
-    uint16_t *buf = r->buf;
-    size_t len = r->len;
-    Message *m = r->message;
+    uint16_t *buf = r->buf_current;
+    size_t len = r->len_current;
+    Message *m = r->message_current;
 
 read:
     size_t n = message_read_from_buffer(m, buf, len);
@@ -13,9 +13,11 @@ read:
     }
 
 next_buf:
-    struct buf_item b = message_reader_next_buffer(r); /* ? */
-    buf = b.buf;
-    len = b.len;
+    if (message_reader_next_buffer(r)) { /* ? */
+        buf = r->buf_current;
+        len = r->len_current;
+    } else {
+        
 
     if (message_is_complete(m)) {
         goto save_new;
@@ -24,8 +26,33 @@ next_buf:
     }
 
 save_new:
-    r->buf = buf;
-    r->len = len;
-    r.message = message_new(); /* what if NULL */
+    r->buf_current = buf;
+    r->len_current = len;
+    r.message_current = message_new(); /* what if NULL */
     return m;
+}
+
+/*---- no goto -----------------------------------------------------*/
+
+struct work_item {
+    Message *m;
+    const uint16_t *buf;
+    size_t len;
+};
+
+Message *message_reader_get_message(MessageReader *r)
+{
+    /* load state */
+    struct work_item *w = r->current_work;
+    if (!w) { return NULL; }
+    const uint16_t *buf = w->buf;
+    size_t len = w->len;
+    Message *m = w->m;
+    if (!buf || !len || !m) { return NULL; } /* or make sure this cannot happen */
+
+    size_t n = message_read_from_buffer(m, buf, len);
+    if (n < len) {
+        buf += n;
+        len -= n;
+    }
 }
