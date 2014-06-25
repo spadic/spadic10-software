e760cfc4e9cb4aa9dd61fe8d47f3f054e45c7cc5
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-25 17:22:51 +0200

    adjusted implementation of Message class (= create derived class)

--- lib/message/wrap/cpp/Message.cpp
+++ lib/message/wrap/cpp/Message.cpp
@@ -4,108 +4,62 @@ extern "C" {
 #include "message.h"
 }
 
+#define IS(NAME) bool is_##NAME() { return message_is_##NAME(m); }
+#define GET(NAME) NAME() { return message_get_##NAME(m); }
+
 namespace spadic {
 
-Message::Message(_Message *m) : m(m), _samples_valid(false)
+struct Message_ : Message {
+    IS (valid)
+    IS (hit)
+    IS (hit_aborted)
+    IS (buffer_overflow)
+    IS (epoch_marker)
+    IS (epoch_out_of_sync)
+    IS (info)
+
+    uint8_t GET (group_id)
+    uint8_t GET (channel_id)
+    uint16_t GET (timestamp)
+    const std::vector<int16_t>& samples() { return _samples; }
+    uint8_t GET (hit_type)
+    uint8_t GET (stop_type)
+    uint8_t GET (buffer_overflow_count)
+    uint16_t GET (epoch_count)
+    uint8_t GET (info_type)
+
+    Message_(struct message *m);
+    ~Message_();
+
+private:
+    struct message *m;
+    std::vector<int16_t> _samples;
+    void init_samples();
+};
+
+Message_::Message_(struct message *m) : m(m)
 {
+    if (is_hit()) {
+        init_samples();
+    }
 }
 
-Message::~Message()
+Message_::~Message_()
 {
     message_delete(m);
 }
 
-//--------------------------------------------------------------------
-
-bool Message::is_valid()
+void Message_::init_samples()
 {
-    return message_is_valid(m);
-}
-
-bool Message::is_hit()
-{
-    return message_is_hit(m);
-}
-
-bool Message::is_hit_aborted()
-{
-    return message_is_hit_aborted(m);
-}
-
-bool Message::is_buffer_overflow()
-{
-    return message_is_buffer_overflow(m);
-}
-
-bool Message::is_epoch_marker()
-{
-    return message_is_epoch_marker(m);
-}
-
-bool Message::is_epoch_out_of_sync()
-{
-    return message_is_epoch_out_of_sync(m);
-}
-
-bool Message::is_info()
-{
-    return message_is_info(m);
-}
-
-//--------------------------------------------------------------------
-
-uint8_t Message::group_id()
-{
-    return message_get_group_id(m);
-}
-
-uint8_t Message::channel_id()
-{
-    return message_get_channel_id(m);
-}
-
-uint16_t Message::timestamp()
-{
-    return message_get_timestamp(m);
-}
-
-const std::vector<int16_t>& Message::samples()
-{
-    if (!_samples_valid) {
-        int16_t *s;
-        size_t n;
-        if ((s = message_get_samples(m)) &&
-            (n = message_get_num_samples(m))) {
-            _samples.assign(s, s+n);
-        }
-        _samples_valid = true;
+    int16_t *s;
+    size_t n;
+    if ((s = message_get_samples(m)) &&
+        (n = message_get_num_samples(m))) {
+        _samples.assign(s, s+n);
     }
-    return _samples;
 }
 
-uint8_t Message::hit_type()
-{
-    return message_get_hit_type(m);
-}
-
-uint8_t Message::stop_type()
-{
-    return message_get_stop_type(m);
-}
-
-uint8_t Message::buffer_overflow_count()
-{
-    return message_get_buffer_overflow_count(m);
-}
-
-uint16_t Message::epoch_count()
-{
-    return message_get_epoch_count(m);
-}
-
-uint8_t Message::info_type()
-{
-    return message_get_info_type(m);
-}
+#undef IS
+#undef GET
 
 } // namespace
