93ff4d9e590d308fad03dc57f9c866c9b7dc6f9c
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-06-25 17:11:06 +0200

    created interface by making all member functions pure virtual, + virtual destructor (see http://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Interface_Class)

--- lib/message/wrap/cpp/Message.h
+++ lib/message/wrap/cpp/Message.h
@@ -4,36 +4,28 @@
 #include <cstdint>
 #include <vector>
 
-typedef struct message _Message;
-
 namespace spadic {
 
 struct Message {
-    ~Message();
+    virtual bool is_valid() = 0;
+    virtual bool is_hit() = 0;
+    virtual bool is_hit_aborted() = 0;
+    virtual bool is_buffer_overflow() = 0;
+    virtual bool is_epoch_marker() = 0;
+    virtual bool is_epoch_out_of_sync() = 0;
+    virtual bool is_info() = 0;
 
-    bool is_valid();
-    bool is_hit();
-    bool is_hit_aborted();
-    bool is_buffer_overflow();
-    bool is_epoch_marker();
-    bool is_epoch_out_of_sync();
-    bool is_info();
+    virtual uint8_t group_id() = 0;
+    virtual uint8_t channel_id() = 0;
+    virtual uint16_t timestamp() = 0;
+    virtual const std::vector<int16_t>& samples() = 0;
+    virtual uint8_t hit_type() = 0;
+    virtual uint8_t stop_type() = 0;
+    virtual uint8_t buffer_overflow_count() = 0;
+    virtual uint16_t epoch_count() = 0;
+    virtual uint8_t info_type() = 0;
 
-    uint8_t group_id();
-    uint8_t channel_id();
-    uint16_t timestamp();
-    const std::vector<int16_t>& samples();
-    uint8_t hit_type();
-    uint8_t stop_type();
-    uint8_t buffer_overflow_count();
-    uint16_t epoch_count();
-    uint8_t info_type();
-
-private:
-    Message(_Message *m);
-    _Message *m;
-    std::vector<int16_t> _samples;
-    bool _samples_valid;
+    virtual ~Message() {};
 };
 
 } // namespace
