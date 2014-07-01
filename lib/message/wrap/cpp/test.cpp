59eae0c5e37a045e099f153606973eba4fbd10fd
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-07-01 15:41:45 +0200

    restructured test.cpp to catch an exception

--- lib/message/wrap/cpp/test.cpp
+++ lib/message/wrap/cpp/test.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include "Message.h"
 
-int main()
+uint16_t buf[8] = {
+    0x8012, /* start of message */
+    0x9666,
+    0xA008,
+    0x0403,
+    0x0100,
+    0x5030,
+    0x0E00,
+    0xB1D0, /* end of message */
+};
+
+void read()
 {
     spadic::MessageReader r;
-
-    uint16_t buf[8] = {
-     0x8012, /* start of message 1 */
-     0x9666,
-     0xA008,
-     0x0403,
-     0x0100,
-     0x5030,
-     0x0E00,
-     0xB1D0, /* end of message 1 */
-    };
 
     r.add_buffer(buf, 8);
 
@@ -26,5 +26,14 @@ int main()
     }
 }
 
+int main()
+{
+    try {
+        read();
+    }
+    catch (spadic::MessageError& e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
