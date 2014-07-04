9e815f4d67135e2318a58e28af00467249b67541
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-07-04 15:47:44 +0200

    use uniform initialization syntax {}

--- lib/message/wrap/cpp/Message.cpp
+++ lib/message/wrap/cpp/Message.cpp
@@ -74,12 +74,12 @@ void Message_::init_samples()
 
 struct MessageReader::MessageReader_ {
     struct message_reader *r;
-    MessageReader_() : r(message_reader_new())
+    MessageReader_() : r {message_reader_new()}
         { if (!r) throw MessageError("Could not create MessageReader."); };
     ~MessageReader_() { message_reader_delete(r); };
 };
 
-MessageReader::MessageReader() : r(new MessageReader_) {}
+MessageReader::MessageReader() : r {new MessageReader_} {}
 MessageReader::~MessageReader() {}
 
 void MessageReader::reset()
@@ -110,7 +110,7 @@ std::unique_ptr<Message> MessageReader::get_message()
     } else {
         throw MessageError("Could not create Message.");
     }
-    return std::unique_ptr<Message>(M);
+    return std::unique_ptr<Message> {M};
 }
 
 } // namespace
