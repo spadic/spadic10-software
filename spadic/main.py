c4f6e91c220d607a14283c62df1b599e5058db3d
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2016-12-15 20:41:05 +0100

    move the logic for accessing the register file over CBMnet into a separate class

--- spadic/main.py
+++ spadic/main.py
@@ -8,9 +8,76 @@ from .shiftregister import SpadicShiftRegister
 from .control import SpadicController
 
 
-# CBMnet control port <-> register file read/write commands
-RF_WRITE = 1
-RF_READ  = 2
+class SpadicCbmnetRegisterAccess:
+    """Read and write registers using the CBMnet control port."""
+
+    WRITE = 1
+    READ  = 2
+
+    from .util import log as _log
+    def _debug(self, *text):
+        self._log.info(' '.join(text))
+
+    def __init__(self, cbmnet):
+        self._cbmnet = cbmnet
+        self._ctrl_queue = IndexQueue()
+        self._setup_thread()
+
+    def __enter__(self):
+        self._start_thread()
+        return self
+
+    def __exit__(self, *args):
+        self._stop_thread()
+
+    def _setup_thread(self):
+        self._stop = threading.Event()
+        self._thread = threading.Thread(name='ctrl worker')
+        self._thread.run = self._ctrl_job
+        self._thread.daemon = True
+
+    def _start_thread(self):
+        self._thread.start()
+
+    def _stop_thread(self):
+        if not self._stop.is_set():
+            self._stop.set()
+        while self._thread.is_alive():
+            self._thread.join(timeout=1)
+        self._debug(self._thread.name, 'finished')
+
+    def _ctrl_job(self):
+        """Process control response received from the CBMnet interface."""
+        while not self._stop.is_set():
+            words = self._cbmnet.read_ctrl()
+            if not words:
+                continue
+            reg_addr, reg_val = words
+            self._ctrl_queue.put(reg_addr, reg_val)
+
+    def write_register(self, address, value):
+        """Write a value into a register."""
+        words = [type(self).WRITE, address, value]
+        self._cbmnet.write_ctrl(words)
+
+    def read_register(self, address, clear_skip=False,
+                      request_skip=False, request_only=False):
+        """Read the value from a register."""
+        if not request_skip:
+            if not clear_skip:
+                # Workaround for retransmit bug in SPADIC 1.0 CBMnet:
+                # Sometimes old register reads are retransmitted -> Clear
+                # software read buffer before sending the read request to be
+                # sure to get the newest value.
+                self._ctrl_queue.clear(address)
+                # End workaround
+            words = [type(self).READ, address, 0]
+            self._cbmnet.write_ctrl(words)
+        if not request_only:
+            try:
+                return self._ctrl_queue.get(address, timeout=1)
+            except IOError:
+                raise
 
 
 class Spadic:
@@ -28,6 +95,7 @@ class Spadic:
 
     def __init__(self, reset=False, load=None, **kwargs):
         self._cbmif = ftdi_cbmnet.FtdiCbmnet()
+        self._reg_access = SpadicCbmnetRegisterAccess(self._cbmif)
 
         self.readout_enable(0)
 
@@ -35,7 +103,6 @@ class Spadic:
         self._dataB_splitter = MessageSplitter()
         self._dataA_queue = queue.Queue()
         self._dataB_queue = queue.Queue()
-        self._ctrl_queue = IndexQueue()
 
         self._setup_threads()
         self._start_threads()
@@ -43,17 +110,17 @@ class Spadic:
         # higher level register file access
         def rf_write_gen(name, addr):
             def write(value):
-                self.write_register(addr, value)
+                self._reg_access.write_register(addr, value)
             return write
         def rf_read_gen(name, addr):
             def read():
-                return self.read_register(addr)
+                return self._reg_access.read_register(addr)
             return read
         self._registerfile = SpadicRegisterFile(rf_write_gen, rf_read_gen)
 
         # higher level shift register access
-        self._shiftregister = SpadicShiftRegister(self.write_register,
-                                                  self.read_register)
+        self._shiftregister = SpadicShiftRegister(
+            self._reg_access.write_register, self._reg_access.read_register)
 
         # highest level configuration controller
         self.control = SpadicController(self._registerfile,
@@ -63,18 +130,20 @@ class Spadic:
 
     def __enter__(self):
         self._cbmif.__enter__()
+        self._reg_access.__enter__()
         return self
 
     def __exit__(self, *args):
         self._stop_threads()
+        self._reg_access.__exit__()
         self._cbmif.__exit__(*args)
 
 
     def _setup_threads(self):
         self._stop = threading.Event()
 
-        names = ['dataA worker', 'dataB worker', 'ctrl worker']
-        jobs = [self._dataA_job, self._dataB_job, self._ctrl_job]
+        names = ['dataA worker', 'dataB worker']
+        jobs = [self._dataA_job, self._dataB_job]
         self._threads = {n: threading.Thread(name=n) for n in names}
 
         for name, job in zip(names, jobs):
@@ -113,42 +182,7 @@ class Spadic:
             for m in self._dataB_splitter(words):
                 self._dataB_queue.put(m)
 
-    def _ctrl_job(self):
-        """Process control response received from the CBMnet interface."""
-        while not self._stop.is_set():
-            words = self._cbmif.read_ctrl()
-            if not words:
-                continue
-            reg_addr, reg_val = words
-            self._ctrl_queue.put(reg_addr, reg_val)
 
-
-    #----------------------------------------------------------------
-    # access register file
-    #----------------------------------------------------------------
-    def write_register(self, address, value):
-        """Write a value into a register."""
-        words = [RF_WRITE, address, value]
-        self._cbmif.write_ctrl(words)
-
-    def read_register(self, address, clear_skip=False,
-                      request_skip=False, request_only=False):
-        """Read the value from a register."""
-        if not request_skip:
-            if not clear_skip:
-                # Workaround for retransmit bug in SPADIC 1.0 CBMnet:
-                # Sometimes old register reads are retransmitted -> Clear
-                # software read buffer before sending the read request to be
-                # sure to get the newest value.
-                self._ctrl_queue.clear(address)
-                # End workaround
-            words = [RF_READ, address, 0]
-            self._cbmif.write_ctrl(words)
-        if not request_only:
-            try:
-                return self._ctrl_queue.get(address, timeout=1)
-            except IOError:
-                raise
         
 
     #----------------------------------------------------------------
