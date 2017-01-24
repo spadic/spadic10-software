7a1d17b8898f794269a1fac49566e42c423fa38f
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2017-01-24 17:14:00 +0100

    make functions into a class analogous to FtdiCbmnetInterface

--- spadic/ftdi_stsxyter.py
+++ spadic/ftdi_stsxyter.py
@@ -1,28 +1,51 @@
 from . import stsxyter_frame
+from .Ftdi import FtdiContainer
+from .mux_stream import MultiplexedStreamInterface, NoDataAvailable
 
 
 class NoUplinkFrame(ValueError):
     """Raised when bytes don't contain any known uplink frame."""
     pass
 
-def convert_uplink_frame(bytes_):
-    """Given an array of 3 bytes, return the contained uplink frame.
 
-    If no type of uplink frame matches the data, raise NoUplinkFrame.
-    """
-    for frame_type in [
-        stsxyter_frame.UplinkSpadicData,
-        stsxyter_frame.UplinkAck,
-        stsxyter_frame.UplinkReadData
-    ]:
+class FtdiStsxyterInterface(FtdiContainer, MultiplexedStreamInterface):
+    """Representation of the FTDI <-> STS-XYTER protocol interface."""
+
+    _uplink_frame_types = [
+        ('HIT', stsxyter_frame.UplinkSpadicData),
+        ('ACK', stsxyter_frame.UplinkAck),
+        ('READ', stsxyter_frame.UplinkReadData)
+    ]
+
+    def write(self, value, destination=None):
+        """Send a downlink frame over FTDI."""
+        downlink_frame = value
+        self._debug('write {!r}'.format(downlink_frame))
+        self._ftdi.write(bytes(downlink_frame))
+
+    def read(self):
+        """Read an uplink frame over FTDI.
+
+        If successful, return a pair (frame type, frame).
+        Otherwise, raise NoDataAvailable.
+        """
+        def convert(data):
+            for name, type_ in self._uplink_frame_types:
+                try:
+                    return name, type_.from_bytes(data, 'big')
+                except stsxyter_frame.PrefixError:
+                    continue
+            else:
+                raise NoUplinkFrame('Bytes contain no uplink frame: {!r}'
+                                    .format(data))
+
+        data = self._ftdi.read(3, max_iter=1)
+        if not data:
+            raise NoDataAvailable
         try:
-            return frame_type.from_bytes(bytes_, 'big')
-        except stsxyter_frame.PrefixError:
-            continue
-    else:
-        raise NoUplinkFrame('Bytes contain no uplink frame: {!r}'
-                            .format(bytes_))
+            type_name, frame = convert(data)
+        except NoUplinkFrame:
+            raise NoDataAvailable
 
-def read_uplink_frame(ftdi):
-    """Given an Ftdi instance, read bytes and return contained frames."""
-    return convert_uplink_frame(ftdi.read(3))
+        self._debug('read {!r}'.format(frame))
+        return type_name, frame
