572e8d506c12a1b8c5f66c89bee3b602940e7c52
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2017-01-27 19:00:31 +0100

    convert to class representing register access using an instance of FtdiStsxyter

--- spadic/downlink_frame.py
+++ spadic/downlink_frame.py
@@ -1,15 +1,24 @@
-from stsxyter_frame import DownlinkFrame, RequestType
+from .stsxyter_frame import DownlinkFrame, RequestType
 
-def write_registers(ftdi, chip_address, operations):
-    """Given an Ftdi instance, send bytes to write registers."""
-    def write_register_frames(chip_address, reg_address, value):
-        # Any two consecutive sequence numbers will be accepted by SPADIC 2.0.
-        for seq, request,             payload in [
-           (0,   RequestType.WR_ADDR, reg_address),
-           (1,   RequestType.WR_DATA, value)
-        ]:
-            yield DownlinkFrame(chip_address, seq, request, payload)
+class SpadicStsxyterRegisterAccess:
+    """Read and write SPADIC registers using the STS-XYTER interface."""
 
-    for reg_address, value in operations:
-        for frame in write_register_frames(chip_address, reg_address, value):
-            ftdi.write(bytes(frame))
+    def __init__(self, stsxyter, chip_address):
+        self._stsxyter = stsxyter
+        self._chip_address = chip_address
+
+    def write_registers(self, operations):
+        """Perform register write operations as specified in the given list of
+        (address, value) tuples.
+        """
+        def write_register_frames(reg_address, value):
+            # Any two consecutive sequence numbers will be accepted by SPADIC 2.0.
+            for seq, request,             payload in [
+               (0,   RequestType.WR_ADDR, reg_address),
+               (1,   RequestType.WR_DATA, value)
+            ]:
+                yield DownlinkFrame(self._chip_address, seq, request, payload)
+
+        for reg_address, value in operations:
+            for frame in write_register_frames(reg_address, value):
+                self._stsxyter.write(frame)
