e9b70256cf8cc794720365aadc994527a581e22b
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2017-02-01 13:12:42 +0100

    cycle through all sequence numbers

--- spadic/stsxyter.py
+++ spadic/stsxyter.py
@@ -1,3 +1,5 @@
+from itertools import cycle
+
 from .stsxyter_frame import DownlinkFrame, RequestType
 
 class SpadicStsxyterRegisterAccess:
@@ -6,18 +8,24 @@ class SpadicStsxyterRegisterAccess:
     def __init__(self, stsxyter, chip_address):
         self._stsxyter = stsxyter
         self._chip_address = chip_address
+        def sequence_number_count():
+            size = DownlinkFrame._fields['sequence_number']
+            return 2 ** size
+        self._sequence_numbers = cycle(range(sequence_number_count()))
 
     def write_registers(self, operations):
         """Perform register write operations as specified in the given list of
         (address, value) tuples.
         """
         def write_register_frames(reg_address, value):
-            # Any two consecutive sequence numbers will be accepted by SPADIC 2.0.
-            for seq, request,             payload in [
-               (0,   RequestType.WR_ADDR, reg_address),
-               (1,   RequestType.WR_DATA, value)
+            for request, payload in [
+                (RequestType.WR_ADDR, reg_address),
+                (RequestType.WR_DATA, value)
             ]:
-                yield DownlinkFrame(self._chip_address, seq, request, payload)
+                yield DownlinkFrame(
+                    self._chip_address, next(self._sequence_numbers),
+                    request, payload
+                )
 
         for reg_address, value in operations:
             for frame in write_register_frames(reg_address, value):
